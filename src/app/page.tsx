c2ee390275b07f7d3498d38397e214e6faf0adbb
DenisFeofanov <omail4me@gmail.com>
2023-11-15 15:36:12 +0500

    [feat] add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,40 @@
 "use client";
 
 import Card from "@/components/Card";
+import PaginationButton from "@/components/PaginationButton";
 import { Character as CharacterInterface } from "@/interfaces/Character";
 import { useEffect, useState } from "react";
+
+interface PaginationURLs {
+  previous: string | null;
+  next: string | null;
+}
 
 const BASE_URL = "https://swapi.dev/api";
 
 export default function Home() {
   const [characters, setCharacters] = useState<CharacterInterface[]>([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [paginationURLs, setPaginationURLs] = useState<PaginationURLs>({
+    previous: null,
+    next: null,
+  });
 
-  function fetchCharacters() {
+  function fetchCharacters(URL: string = `${BASE_URL}/people`) {
     let ignore = false;
 
     async function startFetching() {
       setIsFetching(true);
-      const response = await fetch(`${BASE_URL}/people`);
+      // throw error
+      const response = await fetch(URL);
+      // throw error
       const parsedResponse = await response.json();
 
       if (!ignore) {
+        const { previous, next } = parsedResponse;
+
         setIsFetching(false);
+        setPaginationURLs({ previous, next });
         setCharacters(parsedResponse.results);
       }
     }
@@ -35,30 +50,54 @@ export default function Home() {
   useEffect(fetchCharacters, []);
 
   return (
-    <main className="min-h-screen p-24">
-      <h1 className="font-bold text-5xl">Star Wars characters</h1>
+    <main className="min-h-screen p-24 flex flex-col justify-between">
+      <div>
+        <h1 className="font-bold text-5xl">Star Wars characters</h1>
 
-      <section className="mt-16">
-        <button
-          className="border hover:border-black p-2"
-          type="button"
-          onClick={fetchCharacters}
-        >
-          Reload characters
-        </button>
+        {isFetching ? (
+          <h3 className="mt-16">Fetching users...</h3>
+        ) : (
+          <>
+            <button
+              className="border hover:border-black p-2 mt-16"
+              type="button"
+              onClick={() => fetchCharacters()}
+            >
+              Reload characters
+            </button>
 
-        <ul className="mt-4">
-          {isFetching ? (
-            <p>Fetching users...</p>
-          ) : (
-            characters.map(character => (
-              <li key={character.name} className="my-4">
-                <Card character={character} />
-              </li>
-            ))
-          )}
-        </ul>
-      </section>
+            <ul className="mt-4">
+              {characters.map(character => (
+                <li key={character.name} className="my-4">
+                  <Card character={character} />
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+
+      {!isFetching && (
+        <div className="flex gap-2">
+          <PaginationButton
+            onClick={() =>
+              paginationURLs.previous &&
+              fetchCharacters(paginationURLs.previous)
+            }
+            disabled={!paginationURLs.previous}
+          >
+            Previous page
+          </PaginationButton>
+          <PaginationButton
+            onClick={() =>
+              paginationURLs.next && fetchCharacters(paginationURLs.next)
+            }
+            disabled={!paginationURLs.next}
+          >
+            Next page
+          </PaginationButton>
+        </div>
+      )}
     </main>
   );
 }
